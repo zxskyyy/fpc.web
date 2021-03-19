7816bd9c5011db46540a206128b3e20ab45cc12d
cfuen <SapoPerroGitHub>
2021-03-19 16:20:23 -0500

    aight this is working

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 
 let csvdata = [];
-let isCsvLoaded = false;
 //data load
 d3.dsv(";", "../fpc.web/data/pets-citizens.csv").then(
-    function(csvtemp){csvdata = csvtemp; console.log(csvdata); isCsvLoaded = true;}
+    function(csvtemp){csvdata = csvtemp; console.log(csvdata);}
 );
 
-while(true){   
-    if (isCsvLoaded){
-        console.log("loaded");
-        break;
-    }
-}
+console.log(csvdata);

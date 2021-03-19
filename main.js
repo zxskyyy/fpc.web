d7f93aab47f27f10a818e5e987c80b89f7d9d0fd
cfuen <SapoPerroGitHub>
2021-03-19 16:02:10 -0500

    little test to manage asynchronous functions

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
 let csvdata = [];
-
-//comment because aparently changes didn't push
-
+let isCsvLoaded = false;
+//data load
 d3.dsv(";", "../fpc.web/data/pets-citizens.csv").then(
-    function(csvtemp){csvdata = csvtemp; console.log(csvdata);}
+    function(csvtemp){csvdata = csvtemp; console.log(csvdata); isCsvLoaded = true;}
 );
+
+while(isCsvLoaded == false){ 
+    //do nothing 
+}
+
+console.log("loaded");

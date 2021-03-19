a8c86340dadd3594b960aeae15ce129329f86063
cfuen <SapoPerroGitHub>
2021-03-19 15:06:00 -0500

    okay, maybe I have to put the log print comand on the callback

--- main.js
+++ main.js
@@ -4,7 +4,5 @@ let csvdata = [];
 //comment because aparently changes didn't push
 
 d3.dsv(";", "../fpc.web/data/pets-citizens.csv").then(
-    function(csvtemp){csvdata = csvtemp;}
+    function(csvtemp){csvdata = csvtemp; console.log(csvdata);}
 );
-
-console.log(csvdata);

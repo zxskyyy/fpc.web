a35c4d094355ce325da9325208c93dd5879fdde1
cfuen <SapoPerroGitHub>
2021-03-20 23:45:39 -0500

    first dev commit, includes the still unstable tableStart(pageSize, pageNumber) function

--- main.js
+++ main.js
@@ -1,8 +1,57 @@
 
 let csvdata = [];
+
 //data load
 d3.dsv(";", "../fpc.web/data/pets-citizens.csv").then(
-    function(csvtemp){csvdata = csvtemp; console.log(csvdata);}
+    function(csvtemp){csvdata = csvtemp; console.log(csvdata+" loaded succesfully"); tableStart(5,1)}
 );
 
 console.log(csvdata);
+
+function tableStart(pageSize, pageNumber){
+    //se borra todo
+    //table gets built normally using the elementsOnPage information
+    for(let i = (pageSize*pageNumber)-1; i<(pageNumber*pageSize)+pageSize; i++){
+        //we create a row
+        let newTableRow = document.createElement("tr");
+        //we create and append all children components to the row
+        //microchip;species;sex;size;potentDangerous;neighborhood
+        let newRowMicrochip = document.createElement("th");
+        newRowMicrochip = csvdata[i].microchip;
+        newTableRow.appendChild(newRowMicrochip);
+        let newRowSpecies = document.createElement("th");
+        newRowSpecies = csvdata[i].species;
+        newTableRow.appendChild(newRowSpecies);
+        let newRowSex = document.createElement("th");
+        newRowSex = csvdata[i].sex;
+        newTableRow.appendChild(newRowSex);
+        let newRowSize = document.createElement("th");
+        newRowSize = csvdata[i].size;
+        newTableRow.appendChild(newRowSize);
+        let newRowPotentDangerous = document.createElement("th");
+        newRowPotentDangerous = csvdata[i].potentDangerous;
+        newTableRow.appendChild(newRowPotentDangerous);
+        let newRowNeighborhood = document.createElement("th");
+        newRowNeighborhood = csvdata[i].neighborhood;
+        newTableRow.appendChild(newRowNeighborhood);
+    }
+
+}
+
+//Code that really doesn't matter bc I'm stupid
+    /*if(elementsOnPage.length == 0){
+        //table gets built normally using the elementsOnPage information
+        let newTableRow = document.createElement("tr");
+        for(let i = 0; i<elementsOnPage.length; i++){
+            //first, we create and append all children components to the row
+            
+        }
+    } else {
+        //we empty the array
+        const arraylength = array.length;
+        for(let i=0; i<arraylength; i++){
+            array.shift();
+        }
+        //and then build it using the elementsOnPage information
+
+    }*/
